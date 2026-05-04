dd2f301b69df3640a99aa4bbc228dfce507d9a4d
AdrishikharChodhury <amiadrishikhar@gmail.com>
2026-05-05 00:09:30 +0530

    Navbar feature added

---
+++ components/NavItems.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+interface navItems {
+  label: string;
+  href: string;
+}
+
+const navItems: navItems[] = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "Companions",
+    href: "/companions",
+  },
+  {
+    label: "My Journey",
+    href: "/my-journey",
+  },
+];
+
+const NavItems = () => {
+  const pathname = usePathname();
+  return (
+    <div className="flex items-center gap-4">
+      {navItems.map((navItem: navItems, idx: number) => (
+        <Link
+          href={navItem.href}
+          key={idx}
+          className={cn(
+            pathname === navItem.href && "text-primary font-semibold",
+          )}
+        >
+          {navItem.label}
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default NavItems;
